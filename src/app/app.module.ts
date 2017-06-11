09e67f5dc3fe575ab0b4822c11a2efd31bbcc035
Erik Tittel <et23@live.de>
2017-06-11 21:01:08 +0200

    introduced Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,28 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }         from './app.component';
+import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroService }          from './hero.service';
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
